a22d47f7390307083f9af69177296d7a27ee207b
kriti <kriti.jindal.37@gmail.com>
2024-01-05 04:40:47 +0530

    minify CSS

--- gruntfile.js
+++ gruntfile.js
@@ -9,13 +9,25 @@ module.exports = function (grunt) {
                     "dest/js/main.min.js": ["src/js/*.js"]
                 }
             }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dest/css',
+                    ext: '.min.css'
+                }]
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
